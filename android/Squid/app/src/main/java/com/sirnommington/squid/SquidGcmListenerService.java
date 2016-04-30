8bfdf8968e0c42706dd121efedcc3e951ec4d63c
Ryan Burbidge <Ryan Burbidge>
2016-04-29 22:11:28 -0700

    - Play sound when action is received and executed - Show toast whenever an error occurs

--- android/Squid/app/src/main/java/com/sirnommington/squid/SquidGcmListenerService.java
+++ android/Squid/app/src/main/java/com/sirnommington/squid/SquidGcmListenerService.java
@@ -1,8 +1,14 @@
 package com.sirnommington.squid;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -26,14 +32,37 @@ public class SquidGcmListenerService extends GcmListenerService {
                 i.setData(Uri.parse("tel:" + data.getString("data")));
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(i);
+            } else {
+                error("Unable to handle data=" + data);
             }
+
+            playSound();
         } catch(Exception e) {
-            Log.e(TAG, "onMessageReceived() failure. data=" + data, e);
-            Toast.makeText(this, "onMessageReceived() failure. data=" + data + ", " + e, Toast.LENGTH_LONG).show();
+            error("onMessageReceived() failure. data=" + data, e);
         }
     }
 
-    public void log(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    private void error(String message) {
+        error(message, null);
+    }
+
+    private void error(final String message, Exception e) {
+        Log.e(TAG, message, e);
+
+        // Show toast on UI thread
+        final Context thisContext = this;
+        new Handler(Looper.getMainLooper()).post(
+            new Runnable() {
+                public void run() {
+                    Toast.makeText(thisContext, message, Toast.LENGTH_LONG).show();
+                }
+            }
+        );
+    }
+
+    private void playSound() {
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(this, notification);
+        r.play();
     }
 }
