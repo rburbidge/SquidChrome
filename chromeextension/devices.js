2bfd19b2827e05f16c9dc8e4d480c9bf26b63878
Ryan Burbidge <Ryan Burbidge>
2016-08-21 21:57:04 -0700

    - Move service code into devices adapter. All requests now send google token as authorization header - Updated web requests to use jquery

---
+++ chromeextension/devices.js
@@ -0,0 +1,59 @@
+var devices = {};
+
+devices.baseUrl = 'http://sirnommington.com';
+//devices.baseUrl = 'http://localhost:3000';
+
+devices.getDevices = function(callback, errorCallback) {
+    this.sendAuthorizedRequest(
+        {
+            type: 'GET',
+            url: this.baseUrl + '/api/devices',
+        },
+        callback,
+        errorCallback);
+};
+
+devices.addDevice = function(callback, errorCallback) {
+    this.sendAuthorizedRequest(
+        {
+            type: 'PUT',
+            url: this.baseUrl + '/api/devices',
+        },
+        callback,
+        errorCallback);
+};
+
+devices.sendUrlToDevice = function(url, callback, errorCallback) {
+    this.sendAuthorizedRequest(
+        {
+            type: 'POST',
+            url: this.baseUrl + '/api/devices/commands',
+            data: JSON.stringify({ url: url }),
+        },
+        callback,
+        errorCallback);
+};
+
+devices.sendAuthorizedRequest = function(settings, callback, errorCallback) {
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        if(chrome.runtime.lastError) {
+            errorCallback(chrome.runtime.lastError);
+            return;
+        }
+
+        // Add authorization header
+        if(!settings.headers) {
+            settings.headers = {};
+        }
+        settings.headers.Authorization = token;
+
+        // Add content type if we are sending data
+        if(settings.data) {
+            settings.headers['Content-Type'] = 'application/json';
+        }
+
+        $.ajax(settings)
+            .done(callback)
+            .fail(errorCallback);
+    });
+};
---
+++ chromeextension/popup.js
@@ -0,0 +1,38 @@
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *     is found.
+ */
+function getCurrentTabUrl(callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+        // Can safely assume that at least one tab is open
+        callback(tabs[0].url);
+    });
+}
+
+function renderStatus(statusText) {
+    document.getElementById('status').textContent = statusText;
+}
+
+function renderError(statusText) {
+    renderStatus('Error: ' + statusText);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getCurrentTabUrl(function(url) {
+        renderStatus('Sending... ' + url);
+
+        devices.sendUrlToDevice(
+            url,
+            function() {
+                renderStatus('Sent');
+            },
+            renderError);
+    });
+});
