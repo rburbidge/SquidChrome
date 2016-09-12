b4d6acd72b20b5e7e6c0c9bccce6455aa1aba620
Ryan Burbidge <Ryan Burbidge>
2016-09-11 21:33:53 -0700

    Updated Google sign-in to silently check if the user is signed in already. If the user is signed in, then they are shown the main activity. Otherwise, they are left on the sign-in activity.

--- android/Squid/app/src/main/java/com/sirnommington/squid/activity/SignInActivity.java
+++ android/Squid/app/src/main/java/com/sirnommington/squid/activity/SignInActivity.java
@@ -1,13 +1,12 @@
 package com.sirnommington.squid.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -15,6 +14,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.sirnommington.squid.MainActivity;
 import com.sirnommington.squid.R;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
@@ -45,22 +46,23 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        // TODO Do something if the user is already connected???
-        mGoogleApiClient.connect();
+        // Execute background task to determine if the user is already signed in
+        new AsyncTask<String, Void, String> () {
+            @Override
+            protected String doInBackground(String... params) {
+                OptionalPendingResult<GoogleSignInResult> pendingResult = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+                handleSignInResult(pendingResult.await());
+                return null;
+            }
+        }.execute();
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
+    public void onClick(View view) {
+        if(view.getId() == R.id.sign_in_button) {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, SIGN_IN_REQUEST_CODE);
         }
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, SIGN_IN_REQUEST_CODE);
     }
 
     @Override
@@ -69,20 +71,22 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
         if (requestCode == SIGN_IN_REQUEST_CODE) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if(!result.isSuccess()) {
-                Log.e(TAG, "Sign in failure");
-                return;
-            }
-
-            Log.i(TAG, "Sign in success!");
-
-            // TODO Tell the user that they are signed in and store the token somewhere so that it can be used to send the GCM token to the server
-            Toast.makeText(this, "Sign in " + (result.isSuccess() ? "success!" : "failure :("), Toast.LENGTH_LONG).show();
-
-            if (result.isSuccess()) {
-                GoogleSignInAccount acct = result.getSignInAccount();
-                Log.i(TAG, "Account retrieved"); // TODO Remove this
-            }
+            this.handleSignInResult(result);
         }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (!result.isSuccess()) {
+            Log.e(TAG, "Sign in failure");
+            return;
+        }
+
+        Log.i(TAG, "Sign in success!");
+
+        // Start the main activity, and close the current one
+        // TODO Store the server ID token somewhere for later use
+        GoogleSignInAccount account = result.getSignInAccount();
+        this.startActivity(new Intent(this, MainActivity.class));
+        this.finish();
     }
 }
