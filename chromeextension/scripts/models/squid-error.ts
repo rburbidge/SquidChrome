477b1c7c920717e67e6575c8218c680b9e940c27
rburbidge <ryan.burbidge@gmail.com>
2017-01-25 22:33:23 -0800

    Made handling of squid errors more robust, as there were cases where the server did not respond where interpreting the SquidError was in intself throwing an error.

--- chromeextension/scripts/models/squid-error.ts
+++ chromeextension/scripts/models/squid-error.ts
@@ -6,9 +6,27 @@ export interface SquidError {
 }
 
 export function createSquidError(obj: any): SquidError {
-    let typed: SquidError = obj as SquidError;
+    // If the object has a code field, then assume it is a SquidError object
+    if(obj.code !== undefined) {
+        let typed: SquidError = obj as SquidError;
 
-    // Error code is transmitted as a string, while Typescript enums are numbers. Grab the numeric version
-    typed.code = SquidErrorCode[typed.code.toString()];
-    return typed;
+        // Error code is transmitted as a string, while Typescript enums are numbers. Grab the numeric version
+        typed.code = SquidErrorCode[typed.code.toString()];
+        return typed;    
+    }
+
+    // Otherwise, the object is not a SquidError
+    let message: string;
+    let request: XMLHttpRequest = obj.target;
+
+    if(request) {
+        message = 'Status code: ' + request.status;
+    } else {
+        message = 'Response was not an XMLHttpRequest';
+    }
+    
+    return {
+        code: SquidErrorCode.Unknown,
+        message: message
+    };
 }
