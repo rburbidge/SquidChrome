c28172afaaf22926ffccaeeaea65eaf1b0aafe9c
rburbidge <ryan.burbidge@gmail.com>
2017-09-11 23:13:54 -0700

    Cleanup and document the app manifest

--- scripts/config.ts
+++ scripts/config.ts
@@ -5,5 +5,4 @@ export class Config {
 
     // public static squidEndpoint: string = 'http://sirnommington.com'; // prod
     public static squidEndpoint: string = 'http://localhost:3000'; // localhost
-    //public static squidEndpoint: string = 'http://71.231.137.10'; // localhost public IP
 }
