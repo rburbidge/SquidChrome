b77cb4491457311a92ba8d728add8e1d9fab573e
rburbidge <ryan.burbidge@gmail.com>
2017-11-25 12:58:04 -0800

    Popup

--- src/content/strings.ts
+++ src/content/strings.ts
@@ -6,4 +6,17 @@ export class Strings {
 
     public signedOutMessage = 'Squid requires you to be signed into Google Chrome';
     public signIn = 'Sign in';
+
+    /**
+     * Strings for the send page pop-up action.
+     */
+    public sendPage = {
+        error: 'An error occurred',
+        noSelectedDevice: 'You have no selected device.',
+        noSelectedDeviceOpeningOptionsPage: 'You have no selected device. Opening options page',  
+        pageCannotBeSent: 'Click this while on a different tab. The options page cannot be sent.',
+        pageCannotBeSentOptions: 'Click this while on a different tab. This page cannot be sent.',
+        sendingTo: (deviceName: string) => `Sending to ${deviceName}...`,
+        sentTo: (deviceName: string) => `Sent to ${deviceName}!`,
+    }
 }
