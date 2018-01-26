8f55128ba2663230f20419d62d209199009ffb29
rburbidge <ryan.burbidge@gmail.com>
2018-01-25 22:27:16 -0800

    Update prod endpoint to HTTPS (#23)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-    public static squidEndpoint: string = 'http://sirnommington.com'; // prod
+    public static squidEndpoint: string = 'https://sirnommington.com'; // prod
     //public static squidEndpoint: string = 'http://localhost:3000'; // localhost
 
     public static gcmSenderId: string = '670316986609';
