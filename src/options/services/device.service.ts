1b3c67f02c8ef6a3d0286dfcfcb360001ac4aa9c
rburbidge <ryan.burbidge@gmail.com>
2018-02-03 17:21:43 -0800

    Use promises properly in device service

--- src/options/services/device.service.ts
+++ src/options/services/device.service.ts
@@ -47,50 +47,44 @@ export class DeviceService {
     }
 
     private sendAuthorizedRequest(relativePath: string, options: RequestOptionsArgs): Promise<Response> {
-        return new Promise<Response>((resolve, reject) => {
-            return ChromeAuthHelper.createAuthHeader()
-                .then((authHeader: string) => {
-                    if (!options.headers) {
-                        options.headers = new Headers();
-                    }
-                    options.headers.append('Cache-Control', 'no-cache');
-                    options.headers.append('Authorization', authHeader);
+        return ChromeAuthHelper.createAuthHeader()
+            .then((authHeader: string) => {
+                if (!options.headers) {
+                    options.headers = new Headers();
+                }
+                options.headers.append('Cache-Control', 'no-cache');
+                options.headers.append('Authorization', authHeader);
 
-                    if(options.body) {
-                        options.headers.append('Content-type', 'application/json');
-                        options.body = JSON.stringify(options.body);
-                    }
+                if(options.body) {
+                    options.headers.append('Content-type', 'application/json');
+                    options.body = JSON.stringify(options.body);
+                }
+            })
+            .then(() => this.http.request(this.baseUrl + relativePath, options).toPromise())
+            .catch((response: Response) => {
+                // Resolve on 302. This indicates that a POST request resulted in no change in storage (e.g. Found)
+                if(response.status === 302) {
+                    return response;
+                }
 
-                    this.http.request(this.baseUrl + relativePath, options)
-                        .toPromise()
-                        .then(resolve)
-                        .catch((response: Response) => {
-                            // Resolve on 302. This indicates that a POST request resulted in no change in storage (e.g. Found)
-                            if(response.status === 302) {
-                                resolve(response);
-                                return;
-                            }
+                let error: ErrorModel;
+                if (response && response.json().codeString) {
+                    error = response.json();
+                }
 
-                            let error: ErrorModel;
-                            if (response && response.json().codeString) {
-                                error = response.json();
-                            }
+                // If the error is not defined, then construct a default error for the caller
+                if(!error) {
+                    let errorMsg: string = 'Response was contained an unknown error';
 
-                            // If the error is not defined, then construct a default error for the caller
-                            if(!error) {
-                                let errorMsg: string = 'Response was contained an unknown error';
+                    console.error(errorMsg);
+                    error = {
+                        code: ErrorCode.Unknown,
+                        codeString: ErrorCode[ErrorCode.Unknown],
+                        message: errorMsg
+                    };
+                }
 
-                                console.error(errorMsg);
-                                error = {
-                                    code: ErrorCode.Unknown,
-                                    codeString: ErrorCode[ErrorCode.Unknown],
-                                    message: errorMsg
-                                };
-                            }
-
-                            reject(error);
-                        });
-                });
-            });   
+                throw error;
+            });
     }
 };
