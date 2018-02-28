a228b4e017585bb98bd1211dee3c90bad1e3e279
rburbidge <ryan.burbidge@gmail.com>
2018-02-27 23:04:56 -0800

    Added test for AboutComponent

---
+++ src/areas/popup/components/about/about.component.spec.ts
@@ -0,0 +1,40 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AboutComponent } from './about.component';
+import { AttributionComponent } from './attribution/attribution.component';
+import { ChromeExtensionLinkDirective } from '../../../common/directives/chrome-ext/link/link.directive';
+import { ChromeService } from '../../services/chrome.service';
+import { loadCss } from '../testing/css-loader';
+import { MockChromeService } from '../../services/testing/chrome.service.mock';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+
+describe('AboutComponent', () => {
+    let chromeService: ChromeService;
+
+    let comp: AboutComponent;
+    let fixture: ComponentFixture<AboutComponent>;
+
+    beforeAll(() => {
+        loadCss([
+            'areas/popup/components/toolbar/toolbar.css',
+            'areas/popup/components/about/about.css',
+            'areas/popup/components/about/attribution/attribution.css']);
+    });
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ AttributionComponent, AboutComponent, ChromeExtensionLinkDirective, ToolbarComponent ],
+            imports: [ RouterTestingModule ],
+            providers: [
+                { provide: ChromeService, useValue: new MockChromeService() }
+            ]
+        })
+        .compileComponents();
+    }));
+
+    it('Template', () => {
+        fixture = TestBed.createComponent(AboutComponent);
+        fixture.detectChanges();
+    });
+});
