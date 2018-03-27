0bdc8f9bbb96f12335d025cea59d4c36b88a7522
rburbidge <ryan.burbidge@gmail.com>
2018-03-26 20:24:12 -0700

    Set the correct Android app URL #52

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,9 +2,12 @@ export class Config {
     public static squidEndpoint: string = 'https://sirnommington.com'; // prod
     //public static squidEndpoint: string = 'http://localhost:3000'; // localhost
 
+    /**
+     * The Google Cloud Messaging sender ID. This is the "project number" defined in the Google Cloud Developer Console,
+     * https://console.cloud.google.com/home/dashboard
+     */
     public static gcmSenderId: string = '670316986609';
 
     public chromeWebStore: string = 'https://chrome.google.com/webstore/detail/squid/gipmiglmamlkehhbcicejmfloehgeklk';
-    // TODO Use correct URL for Google Play Store
-    public googlePlayStore: string = 'https://play.google.com/store';
+    public googlePlayStore: string = 'https://play.google.com/store/apps/details?id=com.sirnommington.squid';
 }
