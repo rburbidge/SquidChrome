963c67ae732a27de40843f28ac2059b8948c673b
rburbidge <ryan.burbidge@gmail.com>
2018-05-06 20:19:57 -0700

    Convert AboutComponent to load /squid/about (#86)
    - Rename InstructionsComponent -> IFrameComponent
    - Use IFrameComponent to display both instructions and about pages from Squid Service
    - Add fade-in to loading of web-based experiences. This eliminates load flickering

--- src/areas/popup/components/common/iframe/iframe.component.ts
+++ src/areas/popup/components/common/iframe/iframe.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Sanitizer, HostListener } from "@angular/core";
+import { Component, OnInit, Sanitizer, HostListener, Input } from "@angular/core";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { Route, ActivatedRoute } from "@angular/router";
+import $ from 'jquery';
 
 import { Strings } from "../../../../../assets/strings/strings";
 import { SettingsService } from "../../../services/settings.service";
 import { Config } from "../../../../../config/config";
-import $ from 'jquery';
 import { SquidMessage } from "../../../../../contracts/squid";
 import { WindowService } from "../../../services/window.service";
 
@@ -12,21 +13,25 @@ import { WindowService } from "../../../services/window.service";
  * Shows instructions content for the app.
  */
 @Component({
-    selector: 'instructions',
-    templateUrl: './instructions.html',
-    styleUrls: [ './instructions.css' ]
+    // Do NOT use 'iframe' because the template for this component includes 'iframe', and that causes a stack overflow
+    // of components
+    selector: 'iframe-component',
+    templateUrl: './iframe.html',
+    styleUrls: [ './iframe.css' ]
 })
-export class InstructionsComponent implements OnInit {
+export class IFrameComponent implements OnInit {
     public contentUrl: SafeUrl;
     public contentHeight: string = "0";
+    public class: string;
 
-    constructor(private readonly window: WindowService, private readonly sanitizer: DomSanitizer) { }
+    constructor(private readonly window: WindowService, private readonly sanitizer: DomSanitizer, private readonly route: ActivatedRoute) { }
 
     @HostListener('window:message', ['$event'])
     public onWindowMessage(ev: MessageEvent): void {
         const message: SquidMessage = ev.data;
         if(message.type == 'heightChanged') {
             this.contentHeight = message.data;
+            this.class = "fade-in";
         }
     }
 
@@ -35,7 +40,7 @@ export class InstructionsComponent implements OnInit {
     }
 
     private createInstructionsUrl(): string {
-        const baseInstructionsUrl = Config.squidEndpoint + "/squid/instructions.html";
+        const baseInstructionsUrl = Config.squidEndpoint + this.route.snapshot.data['squidPath'];
         return baseInstructionsUrl + '?' + $.param(
             {
                 client: 'chrome-ext',
